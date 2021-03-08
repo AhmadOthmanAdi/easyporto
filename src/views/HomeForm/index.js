f30b9a5af56d4364c18b1f5241c7dd0734b4406c
mino-cmd <adiatwork@outlook.com>
2021-03-08 16:07:55 +0000

    Initial commit

---
+++ src/views/HomeForm/index.js
@@ -0,0 +1,124 @@
+import { useEffect, useState } from 'react';
+import { withTheme } from 'styled-components';
+import { ArrowRight } from 'react-iconly';
+import PropTypes from 'prop-types';
+import { Tooltip, Corner } from '@components';
+import { useForm } from 'react-hook-form';
+import rules from '@common/rules';
+import { isEmpty } from 'lodash';
+import { toLowerCase } from '@utils';
+import { useToasts } from '@contexts/toasts';
+import { getIsGithubRateLimited } from '@lib/user-builder';
+import {
+  StyledContainer,
+  StyledForm,
+  LargeLogo,
+  HeroTitle,
+  StyledInput,
+  StyledButton,
+  StyledErrorMessage,
+  ProductHuntContainer,
+  RemainingPortfolios,
+} from './styles';
+
+function HomeForm({ theme }) {
+  const [remainingPortfolios, setRemainingPortfolios] = useState('');
+  const { register, handleSubmit, formState, errors } = useForm({
+    mode: 'onChange',
+  });
+  const { ToastsType, addToastWithTimeout } = useToasts();
+
+  useEffect(async () => {
+    try {
+      const { resources } = await getIsGithubRateLimited(true);
+      const { remaining } = resources.core;
+      setRemainingPortfolios(remaining);
+      if (remaining === 0) {
+        addToastWithTimeout(ToastsType.ERROR, 'Github API rate limit exceeded try again in 1 hour');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const { isValid } = formState;
+
+  const onSubmit = ({ username }) => {
+    if (!username || remainingPortfolios === 0) return;
+    const formattedUsername = toLowerCase(username);
+    if (window !== undefined) window.location = `/portfolio/${formattedUsername}`;
+  };
+
+  return (
+    <StyledContainer>
+      <Corner />
+      <LargeLogo alt="DevCover Logo" src="/devcover-logo.png" />
+      <HeroTitle>Just type your username and watch the magic</HeroTitle>
+      <StyledForm onSubmit={handleSubmit(onSubmit)}>
+        <StyledInput
+          disabled={remainingPortfolios === 0}
+          placeholder="Github username"
+          name="username"
+          type="text"
+          ref={register(rules.username)}
+          error={!isEmpty(errors.username)}
+        />
+        <StyledButton type="submit" disabled={!isValid || remainingPortfolios === 0}>
+          <ArrowRight set="light" primaryColor={theme.bg.default} />
+        </StyledButton>
+      </StyledForm>
+      <StyledErrorMessage>
+        {!isEmpty(errors.username) && errors.username.message}
+      </StyledErrorMessage>
+      <RemainingPortfolios>
+        <Tooltip
+          size="big"
+          position="bottom"
+          content="GitHub limits the number of requests per hour to 60 for unauthenticated users"
+        >
+          <RemainingPortfolios>
+            <p>Available requests</p>
+            <p>
+              {remainingPortfolios}
+              /60
+            </p>
+          </RemainingPortfolios>
+        </Tooltip>
+        {remainingPortfolios === 0 && (
+          <p className="github-rate">
+            Github API rate limit exceeded try again in 1 hour&nbsp;
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://docs.github.com/es/github-ae@latest/rest/reference/rate-limit"
+            >
+              Docs
+            </a>
+          </p>
+        )}
+      </RemainingPortfolios>
+      <ProductHuntContainer>
+        <a
+          href="https://www.producthunt.com/posts/devcover?utm_source=badge-review&utm_medium=badge&utm_souce=badge-devcover#discussion-body"
+          target="_blank"
+          rel="noreferrer"
+          style={{ margin: '1rem 0' }}
+        >
+          <img
+            src="https://api.producthunt.com/widgets/embed-image/v1/review.svg?post_id=287287&theme=dark"
+            alt="Devcover - Easiest way to generate a developer portfolio | Product Hunt"
+            style={{ width: '250px', height: '54px' }}
+            width="250"
+            height="54"
+          />
+        </a>
+      </ProductHuntContainer>
+    </StyledContainer>
+  );
+}
+
+HomeForm.propTypes = {
+  theme: PropTypes.object,
+};
+
+export default withTheme(HomeForm);
